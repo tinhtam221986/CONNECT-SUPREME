93cf36418465baab7c20ce5fb62f43cd2c15f9a5
tinhtam221986 <tinhtam221986@gmail.com>
2026-01-24 20:30:12 +0700

    Update next.config.js
    Signed-off-by: tinhtam221986 <tinhtam221986@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'pub-e63dc11b8830414dae41e7e0f122a7e7.r2.dev', // Link R2 của Sếp
-      'minepi.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-e63dc11b8830414dae41e7e0f122a7e7.r2.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minepi.com',
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
